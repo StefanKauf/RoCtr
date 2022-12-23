bcd053540a97f8f2a79b777b476751f3a9f9bf44
StefanKauf <kaufmannstefan@outlook.com>
2022-12-23 22:43:25 +0100

    Modell Animation fix

--- Animation.py
+++ Animation.py
@@ -10,102 +10,34 @@ from Parameter import *
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from IPython.display import HTML
+from IPython import display
 
 
-'''
-def plot(q1,q2):
+def plot(q1,q2,dt):
     """ Animation
         Params
          --------
         q1:            first joint angle     as vector
         q2:            second joint angle    as vector
-        t:             Time vector
+        dt:            Timestep
 
 
         Returns
         --------
-        dx:       chance of the state as a vektor [dx1, dx2]     
+          
                 
     """
-
-
-    history_len = 500  # how many trajectory points to display
+   
 
     L = l1+l2
     x1 = np.cos(q1)*l1
-    y1 = np.sin(q2)*l1
-
-    x2 = np.cos(q1+q2)*l2 +x1
-    y2 = np.sin(q1+q2)*l2 +y1
-
-    fig = plt.figure(figsize=(5, 4))
-    ax = fig.add_subplot(autoscale_on=False, xlim=(-L, L), ylim=(-L, L))
-    ax.set_aspect('equal')
-    ax.grid()
-
-    line, = ax.plot([], [], 'o-', lw=2)
-    trace, = ax.plot([], [], '.-', lw=1, ms=2)
-    time_template = 'time = %.1fs'
-    time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
-    history_x, history_y = deque(maxlen=history_len), deque(maxlen=history_len)
-
-
-
-
-
-
-    def animate(i):
-        thisx = [0, x1[i], x2[i]]
-        thisy = [0, y1[i], y2[i]]
-
-        if i == 0:
-            history_x.clear()
-            history_y.clear()
-
-        history_x.appendleft(thisx[2])
-        history_y.appendleft(thisy[2])
-
-        line.set_data(thisx, thisy)
-        trace.set_data(history_x, history_y)
-        time_text.set_text(time_template % (i*dt))
-        return line, trace, time_text
-
-
-    ani = animation.FuncAnimation(
-        fig, animate, len(y1), interval=dt*1000, blit=True)
-    plt.show()
-'''
-
+    y1 = np.sin(q1)*l1
     
-
-
-
-def plot(q1,q2):
-    """ Animation
-        Params
-         --------
-        q1:            first joint angle     as vector
-        q2:            second joint angle    as vector
-        t:             Time vector
-
-
-        Returns
-        --------
-        dx:       chance of the state as a vektor [dx1, dx2]     
-                
-    """
-   
-
-    L = l1+l2
-    x1 = np.cos(q1)*l1
-    y1 = np.sin(q2)*l1
-
-    x2 = np.cos(q1+q2)*l2 +x1
-    y2 = np.sin(q1+q2)*l2 +y1
+    x2 = np.add(np.cos(np.add(q1,q2))*l2,x1)
+    y2 = np.add(np.sin(np.add(q1,q2))*l2,y1)
 
    
-    fig = plt.figure(figsize=(5, 4))
+    fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(autoscale_on=False, xlim=(-L, L), ylim=(-L, L))
     ax.set_aspect('equal')
     ax.grid()
@@ -114,16 +46,20 @@ def plot(q1,q2):
     # create objects that will change in the animation. These are
     # initially empty, and will be given new values for each frame
     # in the animation.
-    line1, = ax.plot([], [], 'b', lw=2)     # ax.plot returns a list of 2D line objects
-    line2, = ax.plot([], [], 'r', lw=2)
+    line1, = ax.plot([], [], 'b', lw=4)     # ax.plot returns a list of 2D line objects
+    line2, = ax.plot([], [], 'r', lw=4, ms=2)
     
-    def drawframe(n):
+    
+    def drawframe(n):        
         line1.set_data([0,x1[n]],[0,y1[n]])
         line2.set_data([x1[n],x2[n]],[y1[n],y2[n]])
-        return (line1, line2)
+        return (line1,line2)
 
-    anim = animation.FuncAnimation(fig, drawframe, frames=100, interval=20, blit=True)
-    plt.show()
-    HTML(anim.to_html5_video())
+    anim = animation.FuncAnimation(fig, drawframe, frames=x1.size, interval=dt*1000, blit=True)
+    video = anim.to_html5_video()
+    html = display.HTML(video)
+    display.display(html)
+    plt.close()                   # avoid plotting a spare static plot
 
-   
+
+
--- Sim_2R.py
+++ Sim_2R.py
@@ -7,36 +7,11 @@ Robot Control - Simulation 2R Roboter
 """
 
 import numpy as np
-from scipy.integrate import odeint
+from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
-from Model import model_nlin
-
-def eingang(x,t,k=[1,1]):
-    """ System Input
-        Params
-         --------
-        x:             steady states as [x1,x2]
-        t:             time as int
-        k:             Controler Gain k = [k1,k2]   mit k1,k2 > 0       
-                              
-
-        Returns
-        --------
-        u:              System input   
-                
-    """
-    k1 = k[0]
-    k2 = k[1]
-    x1 = x[0]
-    x2 = x[1]
-
-    #u = -k2*(x2+k1)-x1-x1**2  # Version 1
-    u = -k2*(x2+k1*(x1**2)) - x1 -x1**2 -2*k1*x1**2*(x2+k1*x1**2)
-
-    return u 
-
+from Model import model_nlin, model_nlin_ext
 # set the initial conditions
-x0=[np.pi/2,0,0,np.pi/4]
+x0=[-np.pi/2,0,0,np.pi/4]
 
 # define the discretization points
 t_start = 0
@@ -46,20 +21,23 @@ dt = 1e-3
 t_sim=np.linspace(t_start, t_stop, int((t_stop - t_start) / dt + 1))
 
 
+#scipy.integrate.solve_ivp(fun, t_span, y0, method='RK45', t_eval=None, dense_output=False, events=None, vectorized=False, args=None, **options)
 
-solutionOde=odeint(model_nlin,x0,t_sim)
+#solutionOde=odeint(model_nlin,x0,t_sim)
+solOde=solve_ivp(model_nlin_ext,[t_start,t_stop],x0,method = 'RK45',t_eval = t_sim)
 
-plt.plot(t_sim, solutionOde[:, 0], 'b', label='q_1')
-plt.plot(t_sim, solutionOde[:, 2], 'g', label='q_2')
+
+plt.plot(solOde.t, solOde.y[0]*180/np.pi, 'b', label='q_1')
+plt.plot(solOde.t, solOde.y[2]*180/np.pi, 'g', label='q_2')
 plt.legend(loc='best')
 plt.xlabel('time')
-plt.ylabel('q1(t), q2(t)')
+#plt.ylabel('q1(t), q2(t)')
 plt.grid()
 #plt.savefig('simulation.png')
 plt.show()
 
 
 import Animation as anim
-q1 = solutionOde[:, 0]
-q2 = solutionOde[:, 2]
-anim.plot(q1,q2)
+q1 = solOde.y[0]
+q2 = solOde.y[2]
+anim.plot(q1,q2,dt)
