6d96eba3027703355c874367e2479bf7b1103f8b
StefanKauf <kaufmannstefan@outlook.com>
2022-12-24 14:34:35 +0100

    Modell add eq

--- Model.py
+++ Model.py
@@ -8,8 +8,7 @@ verschiedene 2R Robotermodelle
 #from Kinematik_2R import *
 from numpy import sin,cos
 from Parameter import *
-import copy as cp
-from Kinematik_2R import f_modell, f_modell_ext
+#from Kinematik_2R import f_modell, f_modell_ext
 
 
 def model_nlin(t,x,u=[0,0]):
@@ -81,13 +80,15 @@ def model_nlin_ext(t,x,u=[0,0]):
     u1 = u[0]
     u2 = u[1]
 
-    qdd = f_modell_ext(qd1,qd2,q1,q2,u1,u2)
+    #qdd = f_modell_ext(qd1,qd2,q1,q2,u1,u2)
 
 
     dx[0] = x[1]
-    dx[1] = qdd[0]
+    dx[1] = (R1*(I2 + l1*l_s2*m2*cos(q2) + l_s2**2*m2)*(1.0*R2*l_s2*m2*(g*cos(q1 + q2) + l1*qd1**2*sin(q2)) - km2*r2*u2 + qd2*(B2*R2*r2**2 + kb2*km2)) - R2*(I2 + J2*r2**2 + l_s2**2*m2)*(R1*(g*l_s1*m1*cos(q1) + g*m2*(l1*cos(q1) + l_s2*cos(q1 + q2)) - 1.0*l1*l_s2*m2*qd2*(qd1 + qd2)*sin(q2)) - km1*r1*u1 + qd1*(R1*(B1*r1**2 - 1.0*l1*l_s2*m2*qd2*sin(q2)) + kb1*km1)))/(R1*R2*(I1*I2 + I1*J2*r2**2 + I1*l_s2**2*m2 + I2*J1*r1**2 + I2*J2*r2**2 + I2*l1**2*m2 + I2*l_s1**2*m1 + J1*J2*r1**2*r2**2 + J1*l_s2**2*m2*r1**2 + J2*l1**2*m2*r2**2 + 2*J2*l1*l_s2*m2*r2**2*cos(q2) + J2*l_s1**2*m1*r2**2 + J2*l_s2**2*m2*r2**2 + l1**2*l_s2**2*m2**2*sin(q2)**2 + l_s1**2*l_s2**2*m1*m2))
+    #dx[1] = qdd[0]
     dx[2] = x[3]
-    dx[3] = qdd[1]
+    dx[3] = (R1*(-1.0*R2*l_s2*m2*(g*cos(q1 + q2) + l1*qd1**2*sin(q2)) + km2*r2*u2 - qd2*(B2*R2*r2**2 + kb2*km2))*(I1 + I2 + J1*r1**2 + l1**2*m2 + 2*l1*l_s2*m2*cos(q2) + l_s1**2*m1 + l_s2**2*m2) + R2*(I2 + l1*l_s2*m2*cos(q2) + l_s2**2*m2)*(R1*(g*l_s1*m1*cos(q1) + g*m2*(l1*cos(q1) + l_s2*cos(q1 + q2)) - 1.0*l1*l_s2*m2*qd2*(qd1 + qd2)*sin(q2)) - km1*r1*u1 + qd1*(R1*(B1*r1**2 - 1.0*l1*l_s2*m2*qd2*sin(q2)) + kb1*km1)))/(R1*R2*(I1*I2 + I1*J2*r2**2 + I1*l_s2**2*m2 + I2*J1*r1**2 + I2*J2*r2**2 + I2*l1**2*m2 + I2*l_s1**2*m1 + J1*J2*r1**2*r2**2 + J1*l_s2**2*m2*r1**2 + J2*l1**2*m2*r2**2 + 2*J2*l1*l_s2*m2*r2**2*cos(q2) + J2*l_s1**2*m1*r2**2 + J2*l_s2**2*m2*r2**2 + l1**2*l_s2**2*m2**2*sin(q2)**2 + l_s1**2*l_s2**2*m1*m2))
+    #dx[3] = qdd[1]
 
 
 
